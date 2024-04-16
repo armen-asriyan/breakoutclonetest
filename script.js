80561e8e8ea077d963583d1b04438f17566c055b
Armen <163767813+iry-of@users.noreply.github.com>
2024-04-16 23:09:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,29 @@
 // Get the canvas element and store it in the canvas variable
 const canvas = document.getElementById("gameArea");
-// creating the ctx variable to store the 2D rendering context,
-// the actual tool we can use to paint on the Canvas  
+// Get the 2D rendering context for the canvas
 const ctx = canvas.getContext('2d', { alpha: false });
 
-// PADDLE VARIABLES
-
-// Paddle default width and height
-const paddleWidth = 80;
-const paddleHeight = 2.5;
-// Store paddle's x(horizontal) position in a variable then
-// put it on the center of the canvas
+// Paddle variables
+const paddleWidth = 120;
+const paddleHeight = 10;
 let paddleX = (canvas.width - paddleWidth) / 2;
+let paddleY = canvas.height - paddleWidth;
 
-// BALL VARIABLES
+// Ball variables
+let ballX = 120;
+let ballY = 70;
+let ballSpeedX = -2; // Ball speed along the x-axis
+let ballSpeedY = -1.5; // Ball speed along the y-axis
+let ballWidth = 15;
+let ballHeight = 15;
 
-// variables for the x and y position of the "ball"
-let x = 120;
-let y = 70;
-// variables to change position of the "ball"
-let dx = -2;
-let dy = -1.5;
-
-
-// VARIABLES FOR USER INPUT
-
-// Booleans for keyboard buttons
-let gameStarted = false;
+// Variables for user input
+let isGameStarted = false;
+let isGameOver = false;
 let rightPressed = false;
 let leftPressed = false;
 
-
-// VARIABLES FOR THE BRICKS
-// Brick OBJECT
+// Brick variables
 const brick = {
     rowCount: 8,
     columnCount: 8,
@@ -46,45 +37,70 @@ const brick = {
 
 let animationId;
 
+// Initialize the game
+initializeGame();
 
-canvas.addEventListener('mousedown', startGame);
-canvas.addEventListener('touchstart', startGame);
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
-drawPreGame();
-
-function drawPreGame() {
-    // Clear canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    // Draw a text saying tap to start the game
-    ctx.fillStyle = "white";
-    ctx.font = "20px VT323";
-    ctx.textAlign = "center";
-    ctx.fillText("Tap on the screen to play", canvas.width / 2, canvas.height / 2);
+// Function to initialize the game
+function initializeGame() {
+    canvas.addEventListener('mousedown', startGame);
+    canvas.addEventListener('touchstart', startGame);
+    document.addEventListener("keydown", keyDownHandler, false);
+    document.addEventListener("keyup", keyUpHandler, false);
+    drawPreGame();
 }
 
-function drawGame() {
-    // Clear canvas
+// Function to draw the pre-game screen
+function drawPreGame() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    // Draw the paddle
     ctx.fillStyle = "white";
-    ctx.fillRect(paddleX, 120, paddleWidth, paddleHeight);
+    ctx.font = "35px VT323";
+    ctx.textAlign = "center";
+    ctx.fillText("Click on the screen to play", canvas.width / 2, canvas.height / 2);
+}
 
-    // Draw the "ball"
+
+// Function to draw the game elements
+function drawGame() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawPaddle();
+    paddleControls();
+    drawBall();
+    ballBounce();
+}
+
+// Function to draw the paddle
+function drawPaddle() {
     ctx.fillStyle = "white";
-    ctx.fillRect(x, y, 7, 3);
+    ctx.fillRect(paddleX, paddleY, paddleWidth, paddleHeight);
+}
 
-    // Update ball position based on dx and dy
-    x += dx;
-    y += dy;
+// Function to draw the ball
+function drawBall() {
+    ctx.fillStyle = "white";
+    ctx.fillRect(ballX, ballY, ballWidth, ballHeight);
+}
+
+function ballBounce() {
+    ballX += ballSpeedX;
+    ballY += ballSpeedY;
+
+    if (ballY + ballSpeedY < 0) { // Ball is about to hit the top wal
+        ballSpeedY = -ballSpeedY; // Invert y-direction on top collision
+    } else if (ballY + ballSpeedY > canvas.height) { // Ball is about to hit the bottom wall
+        // gameOver();
+    }
 
 
-    // Bounce from the walls
-    bounce();
+    // Ball is about to hit the left wall
+    if (ballX + ballSpeedX < 0) {
+        ballSpeedX = -ballSpeedX; // Invert x-direction on left collision 
+        // Ball is about to hit the right wall
+    } else if (ballX + ballSpeedX > canvas.width) {
+        ballSpeedX = -ballSpeedX; // Invert x-direction on right collision 
+    }
+}
 
-
+function paddleControls() {
     // Update paddle position based on pressed keys
     if (rightPressed && paddleX < canvas.width - paddleWidth) {
         paddleX += 5; // Adjust speed as needed
@@ -92,29 +108,6 @@ function drawGame() {
         paddleX -= 5; // Adjust speed as needed
     }
 }
-
-
-function bounce() {
-    // Ball is about to hit the top wall
-    if (y + dy < 0) {
-        dy = -dy; // Invert y-direction on top collision
-
-        // Ball is about to hit the bottom wall
-    } else if (y + dy > canvas.height) {
-        // Display Game Over screen
-        // gameOver();
-    }
-    // Ball is about to hit the left wall
-    if (x + dx < 0) {
-        dx = -dx; // Invert x-direction on left collision 
-        // Ball is about to hit the right wall
-    } else if (x + dx > canvas.width) {
-        dx = -dx; // Invert x-direction on right collision 
-    }
-}
-
-// BRICK FUNCIONALITY 
-
 
 function keyDownHandler(event) {
     // Update booleans for pressed keys
