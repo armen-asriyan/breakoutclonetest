ebc3e03b40d38f29ed7ac0389aa28fcb15da529a
Armen <163767813+armen-asriyan@users.noreply.github.com>
2024-05-23 13:49:03 +0200

    "Canvasing" the project
    Starting to transform the project into a Canvas project

--- script.js
+++ script.js
@@ -1,152 +1,84 @@
-// Get the canvas element and store it in the canvas variable
+const background = document.getElementById("background");
+
 const canvas = document.getElementById("gameArea");
-// Get the 2D rendering context for the canvas
-const ctx = canvas.getContext('2d', { alpha: false });
+const ctx = canvas.getContext('2d');
 
-// Paddle variables
-const paddleWidth = 120;
-const paddleHeight = 10;
-let paddleX = (canvas.width - paddleWidth) / 2;
-let paddleY = canvas.height - paddleWidth;
+const audioPrompt = document.getElementById("audio-prompt");
 
-// Ball variables
-let ballX = 120;
-let ballY = 70;
-let ballSpeedX = -2; // Ball speed along the x-axis
-let ballSpeedY = -1.5; // Ball speed along the y-axis
-let ballWidth = 15;
-let ballHeight = 15;
+const yesButton = document.getElementById("yes-button");
+const noButton = document.getElementById("no-button");
 
-// Variables for user input
-let isGameStarted = false;
-let isGameOver = false;
-let rightPressed = false;
-let leftPressed = false;
+const arrow = document.getElementById("arrow");
+const chevron = document.querySelector(".chevron");
 
-// Brick variables
-const brick = {
-    rowCount: 8,
-    columnCount: 8,
-    width: 60,
-    height: 2,
-    padding: 10,
-    offsetTop: 30,
-    offsetLeft: 30,
-    isBroken: false
+const rules = document.getElementById("rules");
+const postman = document.querySelector(".postman");
+
+let intro = new Audio("audio/music-intro.wav");
+let music = new Audio("audio/music.wav");
+let yah = new Audio("audio/yah.wav");
+let done = new Audio("audio/done.wav");
+let next = new Audio("audio/next.wav");
+
+let isMuted = false;
+
+const symbols = [
+  "symbols/clock.png",
+  "symbols/deku.png",
+  "symbols/fierce.png",
+  "symbols/goron.png",
+  "symbols/majora.png",
+  "symbols/mirror.png",
+  "symbols/moon.png",
+  "symbols/ocarina.png",
+  "symbols/zora.png",
+];
+
+window.addEventListener("load", (event) => {
+  backgroundSymbols();
+});
+
+function backgroundSymbols() {
+  const currentImages = background.getElementsByClassName("symbol");
+  if (currentImages.length >= 5) {
+    background.removeChild(currentImages[0]);
+  }
+
+  const img = document.createElement("img");
+  img.src = symbols[Math.floor(Math.random() * symbols.length)];
+  img.style.left = Math.random() * 85 + "%";
+  img.style.top = Math.random() * 85 + "%";
+  img.style.filter = "blur(0.9px)"
+  img.className = "symbol";
+  img.alt = "";
+  background.appendChild(img);
+
+  // Fade-in effect
+  setTimeout(() => {
+    img.style.opacity = 0.8;
+  }, 10);
+
+  // Fade-out effect and removal
+  setTimeout(() => {
+    img.style.opacity = 0;
+    setTimeout(() => {
+      if (img.parentElement) {
+        img.parentElement.removeChild(img);
+      }
+    }, 1000);
+  }, 4000);
 }
 
-let animationId;
+setInterval(backgroundSymbols, 1000);
 
-// Initialize the game
-initializeGame();
+let dialogBoxWidth = 750;
+let dialogBoxHeight = 200;
 
-// Function to initialize the game
-function initializeGame() {
-    canvas.addEventListener('mousedown', startGame);
-    canvas.addEventListener('touchstart', startGame);
-    document.addEventListener("keydown", keyDownHandler, false);
-    document.addEventListener("keyup", keyUpHandler, false);
-    drawPreGame();
-}
+ctx.strokeStyle = "rgba(0 0 0 / 0.8)";
+ctx.fillStyle = "rgba(0 0 0 / 0.8)";
+ctx.beginPath();
 
-// Function to draw the pre-game screen
-function drawPreGame() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "white";
-    ctx.font = "35px VT323";
-    ctx.textAlign = "center";
-    ctx.fillText("Click on the screen to play", canvas.width / 2, canvas.height / 2);
-}
-
-
-// Function to draw the game elements
-function drawGame() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawPaddle();
-    paddleControls();
-    drawBall();
-    ballBounce();
-}
-
-// Function to draw the paddle
-function drawPaddle() {
-    ctx.fillStyle = "white";
-    ctx.fillRect(paddleX, paddleY, paddleWidth, paddleHeight);
-}
-
-// Function to draw the ball
-function drawBall() {
-    ctx.fillStyle = "white";
-    ctx.fillRect(ballX, ballY, ballWidth, ballHeight);
-}
-
-function ballBounce() {
-    ballX += ballSpeedX;
-    ballY += ballSpeedY;
-
-    if (ballY + ballSpeedY < 0) { // Ball is about to hit the top wal
-        ballSpeedY = -ballSpeedY; // Invert y-direction on top collision
-    } else if (ballY + ballSpeedY > canvas.height) { // Ball is about to hit the bottom wall
-        // gameOver();
-    }
-
-
-    // Ball is about to hit the left wall
-    if (ballX + ballSpeedX < 0) {
-        ballSpeedX = -ballSpeedX; // Invert x-direction on left collision 
-        // Ball is about to hit the right wall
-    } else if (ballX + ballSpeedX > canvas.width) {
-        ballSpeedX = -ballSpeedX; // Invert x-direction on right collision 
-    }
-}
-
-function paddleControls() {
-    // Update paddle position based on pressed keys
-    if (rightPressed && paddleX < canvas.width - paddleWidth) {
-        paddleX += 5; // Adjust speed as needed
-    } else if (leftPressed && paddleX > 0) {
-        paddleX -= 5; // Adjust speed as needed
-    }
-}
-
-function keyDownHandler(event) {
-    // Update booleans for pressed keys
-    if (event.key === "ArrowRight") {
-        rightPressed = true;
-    } else if (event.key === "ArrowLeft") {
-        leftPressed = true;
-    }
-}
-
-function keyUpHandler(event) {
-    // Update booleans for released keys
-    if (event.key === "ArrowRight") {
-        rightPressed = false;
-    } else if (event.key === "ArrowLeft") {
-        leftPressed = false;
-    }
-}
-
-function gameOver() {
-    // Clear canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    ctx.fillStyle = "white";
-    ctx.font = "20px VT323";
-    ctx.textAlign = "center";
-    ctx.fillText("You lost, tap to try again", canvas.width / 2, canvas.height / 2);
-}
-
-function gameLoop() {
-    animationId = requestAnimationFrame(gameLoop);
-    drawGame();
-}
-
-function startGame() {
-    gameLoop();
-    canvas.removeEventListener('mousedown', startGame);
-    canvas.removeEventListener('touchstart', startGame);
-
-}
-
-
+// roundRect(x, y, width, height, radii)
+ctx.roundRect(25, canvas.height - dialogBoxHeight - 10, dialogBoxWidth, dialogBoxHeight, [10]);
+ctx.stroke();
+ctx.fill();
